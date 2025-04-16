7effce4d843969a952c7e8cadf86bf3e923e00c3
Alae_Din <ala_mchimcha@um5r.ac.ma>
2025-04-16 09:50:48 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import Home from "./pages/Home.jsx";
+
+function App() {
+
+
+  return (
+    <>
+        <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import KeycloakProviderWrapper from "./auth/KeycloakProvider.jsx";
+
+createRoot(document.getElementById('root')).render(
+
+      <KeycloakProviderWrapper>
+        <App />
+      </KeycloakProviderWrapper>
+)
